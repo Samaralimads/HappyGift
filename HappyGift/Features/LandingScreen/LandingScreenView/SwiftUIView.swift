c7b4dc67167ed98d4b35cc4ffdb2457846abaf31
Sebastien_besse <sebastien_besse@yahoo.com>
2025-11-12 16:49:57 +0100

    init

--- HappyGift/Features/LandingScreen/LandingScreenView/SwiftUIView.swift
+++ HappyGift/Features/LandingScreen/LandingScreenView/SwiftUIView.swift
@@ -13,7 +13,7 @@ struct SwiftUIView: View {
             Color.vert
                 .ignoresSafeArea()
             VStack {
-                //
+                // ignored
             }
         }
     }
